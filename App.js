5541f74056c059c6d47cc1bb09f08782d8168b2f
rafamoliv <rafaoliv.ti@gmail.com>
2021-02-03 13:29:12 -0300

    what time is it

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AdMobBanner } from "expo-ads-admob";
 
 export default function App() {
+  let today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  let current_time = `${hours}:${minutes}`;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <AdMobBanner style={styles.banner1} bannerSize="fullBanner" adUnitID="ca-app-pub-2851827333967207/8640104821" servePersonalizedAds />
+
+      <Text style={styles.text1}>{current_time}</Text>
       <StatusBar style="auto" />
+
+      <AdMobBanner style={styles.banner2} bannerSize="fullBanner" adUnitID="ca-app-pub-2851827333967207/8640104821" servePersonalizedAds />
     </View>
   );
 }
@@ -14,8 +24,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#171515",
+    justifyContent: "center",
+  },
+
+  text1: {
+    marginTop: 25,
+    fontSize: 70,
+    textAlign: "center",
+    color: "#f5ffff",
+  },
+
+  banner1: {
+    position: "absolute",
+    marginTop: 30,
+    top: 0,
+  },
+
+  banner2: {
+    position: "absolute",
+    bottom: 0,
   },
 });
